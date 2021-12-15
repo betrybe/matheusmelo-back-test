f2aaaec9a899cad3ffcbf6759facc683746ba0e0
Matheus Melo <matheus.melo.a@hotmail.com>
2021-12-15 20:50:50 -0300

    fix: :bug: remover a password do retorno de sucesso e enviar o retorno com index "user"
    ref: https://github.com/betrybe/matheusmelo-test#1---crie-um-endpoint-para-o-cadastro-de-usu%C3%A1rios

--- src/api/controllers/user.controller.js
+++ src/api/controllers/user.controller.js
@@ -3,5 +3,6 @@ const createUserService = require('../services/user/create.user.service');
 exports.create = async (request, response) => {
     const { name, email, password } = request.body;
     const user = await createUserService.execute(name, email, password, 'user');
-    return response.status(201).json(user);
+    delete user.password;
+    return response.status(201).json({ user });
 };
